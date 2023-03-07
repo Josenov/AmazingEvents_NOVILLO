e172dde2fb1ff897b97fa41d5954cfe94b6bb21e
Jose Maria Novillo <josenovillo88@gmail.com>
2023-03-07 20:29:06 -0300

    implementacion template en cards de home

--- js/index.js
+++ js/index.js
@@ -2,15 +2,35 @@ import data from "./amazing.js"
 
 
 let events = data.events;
-let cards = " ";
+
+//Variable cards usada en el for of de la linea 31
+/* let cards = " "; */
 
 
-//Aca use el for of para implementarlo y ver como funciona ya que no neceistaba filtrar por fechas con los otros js 
-const divCards = document.getElementById('divCards')
 
-for(let info of events){
-    cards += 
-    `<div class="card" style="width: 18rem;">
+//Implementando template para ver como funciona :)
+const divCards = document.querySelector('#divCards');
+const templateCards = document.querySelector('#template-card').content;
+const fragment = document.createDocumentFragment();
+
+
+
+events.forEach(events=>{
+    templateCards.querySelector('.card-img-top').src = events.image;
+    templateCards.querySelector('.card-title').textContent = events.name;
+    templateCards.querySelector('.card-text').textContent = events.description;
+    const repeatCard = templateCards.cloneNode(true);
+    fragment.appendChild(repeatCard);
+})
+
+divCards.appendChild(fragment)
+
+
+
+//Aca use el for of visto en masterclass para implementarlo y ver como funciona ya que no neceistaba filtrar por fechas con los otros js 
+/* for (let info of events) {
+    cards +=
+        `<div class="card" style="width: 18rem;">
         <img src="${info.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${info.name}</h5>
@@ -19,11 +39,11 @@ for(let info of events){
         </div>
     </div>`
 
-    
 
-} 
 
-divCards.innerHTML=cards
+}
+
+divCards.innerHTML = cards */
 
 
 
